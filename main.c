e14569426fa1fe728ac74c522a4d7291b6ddf5a9
lazrossi <lazrossi@student.42.fr>
2018-04-15 13:56:37 +0800

    rewrote a quick hexa conversion to get back to it

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "./libft/includes/libft.h"
+
+char	*hexadecimal_string(char c)
+{
+	static char *string = NULL;
+
+	if (c && string)
+		return (string = ft_strjoinfree_char_str(c, &string));
+	else if (c)
+	{
+		if (!(string = malloc(sizeof(char) * 1)))
+			ft_myexit("malloc error");
+		else
+			*string = c;
+	}
+	return (string);
+}
+
+void	hexa_conversion(int argument)
+{
+	char hexa[17] = "0123456789abcdef";
+
+	hexadecimal_string(hexa[argument % 16]);
+	if (argument / 16)
+		hexa_conversion(argument / 16);
+}
+
+int main(int ac, char **av)
+{
+	(void)ac;
+	int integer;
+	integer = ft_atoi(av[1]);
+	hexa_conversion(integer);
+	ft_putstr(hexadecimal_string(0));
+}
+/*
+	printf("\nret : %d\n", 	   printf("PR %%d : %d", 19));
+	printf("\nret : %010d\n", 	   printf("PR %%d : %d", 19));
+	printf("\nret : %d\n", 	   printf("PR %%D : %D", 19));		
+	printf("\nret : %d\n", 	   printf("PR %%i : %i", 19));
+	printf("\nret : %d\n", 	   printf("PR %%o : %o", 19));
+	printf("\nret : %d\n", 	   printf("PR %%O : %O", 19));
+	printf("\nret : %d\n", 	   printf("PR %%u : %u", 19));
+	printf("\nret : %d\n", 	   printf("PR %%U : %U", 19));
+	printf("\nret : %d\n", 	   printf("PR %%x : %x", 19));
+	printf("\nret : %d\n", 	   printf("PR %%X : %X", 19));
+	*/
