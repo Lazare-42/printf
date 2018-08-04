b9d9e1bb0c49d0401a2220e131aa551ff06db1ee
Lazare ROSSILLON <lazrossi@e3r3p18.42.fr>
2018-08-04 22:55:20 +0200

    added rights in open() when creating a file to write in

--- printf_srcs/bonus_fd.c
+++ printf_srcs/bonus_fd.c
@@ -6,7 +6,7 @@
 /*   By: lazrossi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/23 15:23:14 by lazrossi          #+#    #+#             */
-/*   Updated: 2018/07/25 13:45:36 by lazrossi         ###   ########.fr       */
+/*   Updated: 2018/08/04 22:54:53 by lazrossi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ void		try_opening(void *path, int o_flag)
 	fd = 0;
 	if (wordexp(path_read, &path_exp, 0))
 		ft_printf("path expansion error\n");
-	else if ((fd = open(*path_exp.we_wordv, o_flag)) < 0)
+	else if ((fd = open(*path_exp.we_wordv, o_flag, S_IRWXU)) < 0)
 	{
 		ft_printf("FD option : Error in opening creating the file : ");
 		ft_printf("%s. Error :  %s\n", path, strerror(errno));
